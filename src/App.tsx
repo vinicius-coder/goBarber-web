72097fea379e299839cbdd55fa5452770388a5bb
Avonts <vinialves022@gmail.com>
2021-04-05 12:36:05 -0300

    Add SignIn with Yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 import GlobalStyles from './styles/global';
 
 const App = () => {
   return (
     <>
-      <SignUp />
+      <SignIn />
       <GlobalStyles />
     </>
   );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Form } from '@unform/web';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 import { FiLock, FiLogIn, FiMail } from 'react-icons/fi';
 
 import { Background, Container, Content } from './styles';
+import { FormHandles } from '@unform/core';
+import getValidationsErrors from '../../Utils/getValidationsErrors';
 
 const SignIn: React.FC = () => {
 
-    function handleSubmit(data: object) {
-        console.log(data);
-    }
+    const formRef = useRef<FormHandles>(null);
+
+    console.log(formRef);
+
+    const handleSubmit = useCallback(async (data: object) => {
+        try {
+            formRef.current?.setErrors({});
+            
+            const schema = Yup.object().shape({
+                email: Yup.string().required('E-mail é obrigatório').email('Digite um e-mail válido'),
+                password: Yup.string().required('Senha é obrigatório'),
+            });
+
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+
+        } catch (err) {
+
+            const errors = getValidationsErrors(err);
+
+            formRef.current?.setErrors(errors);
+        }
+    }, []);
 
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="GoBarber" />
 
-                <Form onSubmit={handleSubmit}>
+                <Form ref={formRef} onSubmit={handleSubmit}>
                     <h1>Faça seu logon</h1>
                     <Input
                         name="email"
