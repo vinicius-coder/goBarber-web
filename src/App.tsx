ff3cf2dd28927a9d0d80e95b78b61523dd229da8
Avonts <vinialves022@gmail.com>
2021-04-06 18:40:24 -0300

    Teste do useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import SignIn from './pages/SignIn';
 import GlobalStyles from './styles/global';
+import { AuthProvider } from './hooks/AuthContext';
 
-const App = () => {
-  return (
+const App = () => (
     <>
-      <SignIn />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
       <GlobalStyles />
     </>
-  );
-}
+)
 
 export default App;
